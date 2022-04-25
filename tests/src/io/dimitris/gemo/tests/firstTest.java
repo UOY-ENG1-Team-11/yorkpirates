61bb7a5ebb213412763cb7f23235bd3f65485ea6
Tomizzed2001 <tj799@york.ac.uk>
2022-04-25 19:53:47 +0100

    Added the capability to test
    Testing has been added along with a brief example test to test the ship1.png file is accessible

---
+++ tests/src/io/dimitris/gemo/tests/firstTest.java
@@ -0,0 +1,14 @@
+package io.dimitris.gemo.tests;
+
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
+@RunWith(GdxTestRunner.class)
+public class firstTest {
+    @Test
+    public void testShipAssetExists() {
+        assertTrue("This test will only pass when the ship.png asset exists.",
+                Gdx.files.internal("ship1.png").exists());
+    }
+}
